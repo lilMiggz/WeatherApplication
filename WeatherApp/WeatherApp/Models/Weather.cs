3515c1dc17283801d8afa4246a648a85a38701dd
Miguel Morell <migg.angel@outlook.com>
2018-08-16 14:28:48 -0400

    Creating Weather model for Weather database

---
+++ WeatherApp/WeatherApp/Models/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public int ID { get; set; }
+        public int ZIPCODE { get; set; }
+        public string LOCATION { get; set; }
+        public string FORECAST { get; set; }
+        public int TEMPERATURE { get; set; }
+
+    }
+
+    public class WeatherDBContext : DbContext {
+        public DbSet<Weather> Weathers { get; set; }
+    }
+}
